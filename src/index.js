4bbca7e6fbaafc0b6c7a0127d9fc45fc0eca3053
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-25 02:54:35 +0800

    Included EventListener for HomePage

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ContactPage from "./Pages/ContactPage.js";
 import OrderPage from "./Pages/OrderPage.js";
 // import Images from "./images/";
 
+const eventListener = () =>{
+    const homebtn = document.getElementById(".homebtn");
+    const orderbtn = document.getElementById(".orderbtbn");
+    const contactbtn = document.getElementById(".contactbtn");
+    homebtn.addEventListener("click", ()=>{
+        HomePage();
+    });
+}
 
 HomePage();
-// Images();
+
+window.load = eventListener;
+
+
