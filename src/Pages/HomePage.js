611f5789b0de99e92a77c83ba8af88b8fcfdd3c8
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-25 02:53:49 +0800

    Created the header, appended the title inside the content and added footer

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,35 +1,52 @@
-const createHome = () =>{
-    const header = document.createElement("header");
-    const iconDiv = document.createElement("div");
-    const selectDiv = document.createElement("div");
-    const homebtn = document.createElement("button");
-    const orderbtn = document.createElement("button");
-    const contactbtn = document.createElement("button");
 
-    iconDiv.classList.add("icon");
-    selectDiv.classList.add("select");
-    homebtn.classList.add("homebtn");
-    orderbtn.classList.add("orderbtn");
-    contactbtn.classList.add("contactbtn");
+const createHome = () => {
+  const header = document.createElement("header");
+  const iconDiv = document.createElement("div");
+  const selectDiv = document.createElement("div");
+  const homebtn = document.createElement("button");
+  const orderbtn = document.createElement("button");
+  const contactbtn = document.createElement("button");
 
-    header.append(iconDiv);
-    header.append(selectDiv);
+  iconDiv.classList.add("icon");
+  selectDiv.classList.add("select");
+  homebtn.classList.add("homebtn");
+  orderbtn.classList.add("orderbtn");
+  contactbtn.classList.add("contactbtn");
 
-    selectDiv.append(homebtn);
-    selectDiv.append(orderbtn);
-    selectDiv.append(contactbtn);
+  header.append(iconDiv);
+  header.append(selectDiv);
 
-    iconDiv.textContent = "icon";
+  selectDiv.append(homebtn);
+  selectDiv.append(orderbtn);
+  selectDiv.append(contactbtn);
 
-    homebtn.textContent ="Home";
-    orderbtn.textContent = "Order";
-    contactbtn.textContent = "Contact";
+  iconDiv.textContent = "icon";
 
-    return header;
-}
+  homebtn.textContent = "Home";
+  orderbtn.textContent = "Order";
+  contactbtn.textContent = "Contact";
 
-const HomePage = () =>{
-    document.body.append(createHome());
-}
+  return header;
+};
 
-export default HomePage; 
+const content = () => {
+  const contentContainer = document.getElementById("content");
+  const title = document.createElement("h1");
+  title.textContent = "Welcome to my Restaurant";
+  contentContainer.append(title);
+  return contentContainer;
+};
+
+const footer = () => {
+  const footer = document.createElement("footer");
+  footer.textContent = "Made by Callistus Anizoba";
+  return footer;
+};
+
+const HomePage = () => {
+  document.body.append(createHome());
+  document.body.append(content());
+  document.body.append(footer());
+};
+
+export default HomePage;
