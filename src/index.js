58bb97d5fb7465fb5a1ad6cfe9505321f2807e1c
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-26 02:36:12 +0800

    Fixed event button for tab switching

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import HomePage from "./Pages/HomePage.js";
 import ContactPage from "./Pages/ContactPage.js";
 import OrderPage from "./Pages/OrderPage.js";
-// import Images from "./images/";
 
-const eventListener = () =>{
-    const homebtn = document.getElementById(".homebtn");
-    const orderbtn = document.getElementById(".orderbtbn");
-    const contactbtn = document.getElementById(".contactbtn");
-    homebtn.addEventListener("click", ()=>{
+const eventListener = () => {
+  const btn = document.querySelectorAll("button");
+  btn.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (e.target.id === "homebtn") {
         HomePage();
+      } else if (e.target.id === "contactbtn") {
+        ContactPage();
+      } else if (e.target.id === "orderbtn") {
+        OrderPage();
+      }
     });
-}
+  });
+};
 
 HomePage();
 
-window.load = eventListener;
-
-
+eventListener();
