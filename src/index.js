4f626469cb673533f485d2d43cbcb1a3d44071d7
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-27 01:16:06 +0800

    tab switch good to go

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ const addEventListener = () => {
   btn.forEach((button) => {
     button.addEventListener("click", (e) => {
       if (e.target.id === "homebtn") {
-        document.getElementById("hometitle").style.display = "flex";
+        document.getElementById("homecontainer").style.display = "block";
         document.getElementById("ordertitle").style.display = "none";
         document.getElementById("contact-title").style.display = "none";
       } else if (e.target.id === "orderbtn") {
         document.getElementById("ordertitle").style.display = "flex";
-        document.getElementById("hometitle").style.display = "none";
+        document.getElementById("homecontainer").style.display = "none";
         document.getElementById("contact-title").style.display = "none";
       } else if (e.target.id === "contactbtn") {
         document.getElementById("contact-title").style.display = "flex";
-        document.getElementById("hometitle").style.display = "none";
+        document.getElementById("homecontainer").style.display = "none";
         document.getElementById("ordertitle").style.display = "none";
       }
     });
