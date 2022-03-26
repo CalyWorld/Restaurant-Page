a12df6889534e4b5c25c8a4845879caeaccc4af4
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-27 01:14:19 +0800

    fixed the contact page bugs

---
+++ src/Pages/ContactPage.js
@@ -0,0 +1,14 @@
+const content = () => {
+  const contentContainer = document.getElementById("content");
+  const title = document.createElement("h1");
+  title.textContent = "Make a Contact";
+  title.setAttribute("id", "contact-title");
+  contentContainer.append(title);
+  return contentContainer;
+};
+
+const ContactPage = () => {
+  document.body.append(content());
+};
+
+export default ContactPage;
