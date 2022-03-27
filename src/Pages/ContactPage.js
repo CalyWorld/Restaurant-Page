4dc99f9ea7b80db53e3294a7f3c60b720f1929e0
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-27 22:37:51 +0800

    Contact Page

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,9 +1,14 @@
 const content = () => {
   const contentContainer = document.getElementById("content");
+  const ContainerDiv = document.createElement("div");
+  ContainerDiv.setAttribute("id", "contactcontainer");
   const title = document.createElement("h1");
+
   title.textContent = "Make a Contact";
   title.setAttribute("id", "contact-title");
-  contentContainer.append(title);
+
+  ContainerDiv.append(title);
+  contentContainer.append(ContainerDiv);
   return contentContainer;
 };
 
