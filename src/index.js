dcaf3c33795503a7837c08f92834f06c4d371116
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-27 22:40:01 +0800

    update index

--- src/index.js
+++ src/index.js
@@ -2,35 +2,33 @@ import HomePage from "./Pages/HomePage.js";
 import ContactPage from "./Pages/ContactPage.js";
 import OrderPage from "./Pages/OrderPage.js";
 
-
-
 const addEventListener = () => {
   const btn = document.querySelectorAll("button");
- 
+
   btn.forEach((button) => {
     button.addEventListener("click", (e) => {
       if (e.target.id === "homebtn") {
         document.getElementById("homecontainer").style.display = "block";
-        document.getElementById("ordertitle").style.display = "none";
-        document.getElementById("contact-title").style.display = "none";
+        document.getElementById("ordercontainer").style.display = "none";
+        document.getElementById("contactcontainer").style.display = "none";
       } else if (e.target.id === "orderbtn") {
-        document.getElementById("ordertitle").style.display = "flex";
+        document.getElementById("ordercontainer").style.display = "block";
         document.getElementById("homecontainer").style.display = "none";
-        document.getElementById("contact-title").style.display = "none";
+        document.getElementById("contactcontainer").style.display = "none";
       } else if (e.target.id === "contactbtn") {
-        document.getElementById("contact-title").style.display = "flex";
+        document.getElementById("contactcontainer").style.display = "block";
         document.getElementById("homecontainer").style.display = "none";
-        document.getElementById("ordertitle").style.display = "none";
+        document.getElementById("ordercontainer").style.display = "none";
       }
     });
   });
 };
 
-const functionHolder = () =>{
+const functionHolder = () => {
   HomePage();
   OrderPage();
   ContactPage();
   addEventListener();
-}
+};
 
 functionHolder();
